3e11d23f5f22992e8266296fb262acd49b701e89
vk2920 <vk2920@yandex.ru>
2020-03-31 11:43:31 +0300

    Добавлен прелоадер Некоторые исправления в первой работе портфолио Надо придумать стиль карточек портфолио, ибо сейчас они выглядят ужасно

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 // Window properties
 win_w = $(window).width();
 win_h = $(window).height();
+console.log("Window: " + win_w + "x" + win_h);
 
 // Save heights of sections
 main_h = $("#main").height();
 features_h = $("#features").height();
 portfolio_h = $("#portfolio").height();
+console.log("Heights of sections contents saved");
+
+porfolio_carousel_w = $("#porfolio-carousel").width();
+portfolio_card_w = porfolio_carousel_w - 288;
+$(".portfolio-card").css("width", portfolio_card_w);
+console.log("Size of portfolio-card is setted to " + portfolio_card_w);
 
 // Paddings for sections
 // MAIN
@@ -19,6 +26,10 @@ portfolio_pt = (win_h - portfolio_h) / 2 + 56;
 $("#portfolio").css("padding-top", portfolio_pt);
 
 $(document).resize(function() {
+  // Update window properties
+  win_w = $(window).width();
+  win_h = $(window).height();
+
   // Paddings for sections
   // MAIN
   main_pt = (win_h - main_h) / 2 + 56;
@@ -37,6 +48,8 @@ $(document).ready(function(){
     var anchor = $(this);
     $('html, body').stop().animate({scrollTop: $(anchor.attr('href')).offset().top - 56}, 600);
     e.preventDefault();
-  })
+  });
+  console.log("Loaded!!!");
+  $('#preloader').fadeOut().end().delay(300).fadeOut('slow');
   return false;
 });
--- portfolio/first-work/js/head.js
+++ portfolio/first-work/js/head.js
@@ -7,45 +7,15 @@ $("html").onload(function () {
 	$("#preloader").addClass("loaded");
 });
 
+// Animated scroll to anchors
 $(document).ready(function(){
-	$('#main').click(function(){
-		// скорость скролла
-		var speed = 1000;
-		// место скролла
-		var top = $('#main').offset().top;
-		$('html, body').animate({scrollTop: top}, speed);
-		return false;
-	});
-	$('#about').click(function(){
-		// скорость скролла
-		var speed = 1000;
-		// место скролла
-		var top = $('#about').offset().top;
-		$('html, body').animate({scrollTop: top}, speed);
-		return false;
-	});
-	$('#faq').click(function(){
-		// скорость скролла
-		var speed = 1000;
-		// место скролла
-		var top = $('#faq').offset().top;
-		$('html, body').animate({scrollTop: top}, speed);
-		return false;
-	});
-	$('#tariff').click(function(){
-		// скорость скролла
-		var speed = 1000;
-		// место скролла
-		var top = $('#tariff').offset().top;
-		$('html, body').animate({scrollTop: top}, speed);
-		return false;
-	});
-	$('#contact').click(function(){
-		// скорость скролла
-		var speed = 1000;
-		// место скролла
-		var top = $('#contact').offset().top;
-		$('html, body').animate({scrollTop: top}, speed);
-		return false;
-	});
+  $('a.internal').bind("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({scrollTop: $(anchor.attr('href')).offset().top - 56}, 600);
+    e.preventDefault();
+  });
+  console.log("Loaded!!!");
+  $("#preloader").addClass("loaded");
+  $("#navbar").removeClass("d-none");
+  return false;
 });
