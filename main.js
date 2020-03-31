1d740be44ca0bf2dc4d117797659c8ce00cf665a
vk2920 <vk2920@yandex.ru>
2020-03-31 12:03:18 +0300

    Убран автопаддинг Изменён внешний вид карточек портфолио (ещё будет дорабатываться)

--- main.js
+++ main.js
@@ -9,21 +9,23 @@ features_h = $("#features").height();
 portfolio_h = $("#portfolio").height();
 console.log("Heights of sections contents saved");
 
-porfolio_carousel_w = $("#porfolio-carousel").width();
-portfolio_card_w = porfolio_carousel_w - 288;
-$(".portfolio-card").css("width", portfolio_card_w);
+if (win_w >= 768) {
+  porfolio_carousel_w = $("#porfolio-carousel").width();
+  portfolio_card_w = porfolio_carousel_w - 288;
+  $(".portfolio-card").css("width", portfolio_card_w);
+}
 console.log("Size of portfolio-card is setted to " + portfolio_card_w);
 
-// Paddings for sections
-// MAIN
-main_pt = (win_h - main_h) / 2 + 56;
-$("#main").css("padding-top", main_pt);
-// FEATURES
-features_pt = (win_h - features_h) / 2 + 56;
-$("#features").css("padding-top", features_pt);
-// PORTFOLIO
-portfolio_pt = (win_h - portfolio_h) / 2 + 56;
-$("#portfolio").css("padding-top", portfolio_pt);
+// // Paddings for sections
+// // MAIN
+// main_pt = (win_h - main_h) / 2 + 56;
+// $("#main").css("padding-top", main_pt);
+// // FEATURES
+// features_pt = (win_h - features_h) / 2 + 56;
+// $("#features").css("padding-top", features_pt);
+// // PORTFOLIO
+// portfolio_pt = (win_h - portfolio_h) / 2 + 56;
+// $("#portfolio").css("padding-top", portfolio_pt);
 
 $(document).resize(function() {
   // Update window properties
